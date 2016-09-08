327ca67c00c09b8583ea0daf46d57cbfed7cf831
Stephen Riddell <stephen.riddell@spectraqest.com>
2016-09-08 18:20:39 +0930

    trying to get size correct

--- experiments/documentpreview/scripts/viewer.js
+++ experiments/documentpreview/scripts/viewer.js
@@ -1,4 +1,20 @@
 !function (w) {
+    function getOutputScale(ctx) {
+        var devicePixelRatio = window.devicePixelRatio || 1;
+        var backingStoreRatio = ctx.webkitBackingStorePixelRatio ||
+                                ctx.mozBackingStorePixelRatio ||
+                                ctx.msBackingStorePixelRatio ||
+                                ctx.oBackingStorePixelRatio ||
+                                ctx.backingStorePixelRatio || 1;
+        var pixelRatio = devicePixelRatio / backingStoreRatio;
+        return {
+            sx: pixelRatio,
+            sy: pixelRatio,
+            scaled: pixelRatio !== 1
+        };
+    }
+    var CSS_UNITS = 96.0 / 72.0; //this is apparently a thing.
+
     PDFJS.getDocument("1609.01714.pdf").then(function (pdf) {
         var pageDivs = [];
         function createPage(n) {
@@ -7,13 +23,20 @@
             }
             var div = document.createElement("div");
             pageDivs.push(div);
-            var canvas = document.createElement("canvas");
-            div.appendChild(canvas);
-            var context = canvas.getContext("2d");
-            canvas.width = 500;
-            canvas.height = Math.round(Math.sqrt(2) * 500);
             pdf.getPage(n).then(function (page) {
-                var viewport = page.getViewport(1);
+                console.log(page.width);
+                console.log(page.height);
+
+                var context = canvas.getContext("2d");
+                var scale = 1;
+                var viewport = page.getViewport(scale * CSS_UNITS);
+
+                var canvas = document.createElement("canvas");
+                var outputScale = getOutputScale(canvas);
+                canvas.height = viewport.height * outputScale.sy;
+                canvas.width = viewport.width * outputScale.sx;
+
+                div.appendChild(canvas);
                 page.render({
                     canvasContext: context,
                     viewport: viewport
