38e65dc6845bdfa005af8e1ebc7c8cbdaf1d0534
Stephen Riddell <stephen.riddell@spectraqest.com>
2016-09-08 15:49:14 +0930

    intial test of some pdf.js fun

---
+++ experiments/documentpreview/scripts/viewer.js
@@ -0,0 +1,29 @@
+!function (w) {
+    PDFJS.getDocument("1609.01714.pdf").then(function (pdf) {
+        var pageDivs = [];
+        function createPage(n) {
+            if (n >= pdf.numPages) {
+                return;
+            }
+            var div = document.createElement("div");
+            pageDivs.push(div);
+            var canvas = document.createElement("canvas");
+            div.appendChild(canvas);
+            var context = canvas.getContext("2d");
+            canvas.width = 500;
+            canvas.height = Math.round(Math.sqrt(2) * 500);
+            pdf.getPage(n).then(function (page) {
+                var viewport = page.getViewport(1);
+                page.render({
+                    canvasContext: context,
+                    viewport: viewport
+                });
+            })
+            createPage(n + 1);
+        }
+        createPage(0);
+        pageDivs.forEach(function (div) {
+            document.body.appendChild(div);
+        });
+    });
+} (window);
